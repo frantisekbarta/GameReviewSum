d8af4238f5b74b2fdcd409d361739fc9c0450de7
frantisekbarta <88767131+frantisekbarta@users.noreply.github.com>
2023-01-10 12:18:33 +0100

    Add files via upload

---
+++ Data/Game.cs
@@ -0,0 +1,20 @@
+﻿namespace ReviewSum.Data
+{
+    public class Game
+    {
+        public string Id { get; set; }
+        public string? Name { get; set; }
+        public string? SquareImage { get; set; }
+        public string? Url { get; set; }
+        public int? PercentRecommended { get; set; }
+        public int? NumReviews { get; set; }
+        public int? NumTopCriticReviews { get; set; }
+        public int? MedianScore { get; set; }
+        public int? TopCriticScore { get; set; }
+        public int? TopReviewsScore { get; set; }
+        public string? Description { get; set; }
+        public List<string>? Companies { get; set; }
+        public List<string>? Platforms { get; set; }
+        public List<string>? Genres { get; set; }
+    }
+}
---
+++ Data/Review.cs
@@ -0,0 +1,18 @@
+﻿namespace ReviewSum.Data
+{
+    public class Review
+    {
+        public string Id { get; set; }
+        public string? Title { get; set; }
+        public DateTime? PublishedDate { get; set; }
+        public string? ExternalUrl { get; set; }
+        public string? Snippet { get; set; }
+        public int? Score { get; set; }
+        public int? Recommended { get; set; }
+        public string? Alias { get; set; }
+        public string? OutletName { get; set; }
+        public string? OutletImage { get; set; }
+        public List<string>? Authors { get; set; }
+        public List<string>? Platforms { get; set; }
+    }
+}
