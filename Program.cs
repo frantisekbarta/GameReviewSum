5b92e64cf24da8cc27e9baacc9bc661c34966d24
frantisekbarta <88767131+frantisekbarta@users.noreply.github.com>
2023-01-21 14:10:05 +0100

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -5,7 +5,7 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 builder.Services.AddRazorPages();
 builder.Services.AddServerSideBlazor();
-builder.Services.AddSingleton<IReviewSumService, ReviewSumServiceTest>();
+builder.Services.AddSingleton<IReviewSumService, ReviewSumService>();
 
 var app = builder.Build();
 
