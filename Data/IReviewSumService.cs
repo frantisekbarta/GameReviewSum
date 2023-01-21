23e3efe02bf69bb5c4a09f38e555f2eb1c2383f7
frantisekbarta <88767131+frantisekbarta@users.noreply.github.com>
2023-01-21 13:58:15 +0100

    Add files via upload

---
+++ Data/IReviewSumService.cs
@@ -0,0 +1,12 @@
+﻿namespace ReviewSum.Data
+{
+    public interface IReviewSumService
+    {
+        Task<List<Game>> SearchGame(string enteredName);
+        Task<Game> GetGame(Game game);
+        Task<List<Review>> GetReviews(string gameId);
+        int GetTopReviewsScore(List<Review> reviews);
+        Task<List<string>> GetSummary(List<Review> reviews);
+        Task<List<string>> GetProsAndCons(List<Review> reviews);
+    }
+}
